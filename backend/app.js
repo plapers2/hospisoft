6b522e26c6b941df310707d5e296a60f8791e21b
plapers2 <mateohoyosgamer@gmail.com>
2026-02-13 18:09:46 -0500

    Rutas de CRUD medicamentos semifinal, (falta aclarar activar y desactivar)

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,15 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 //* Importar rutas
-import pacientesRutas from "./src/routes/pacientes.route.js";
+import medicamentosRutas from "./src/routes/medicamentos.route.js";
 // Instancia global
 const app = express();
 
-
 // Middlewares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 //* Rutas
-app.use("/api",pacientesRutas);
+app.use("/api", medicamentosRutas);
 
-export default app
+export default app;
