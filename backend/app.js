99fb5c66ea190c749274ab1d930700178ad63b30
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-02-15 15:32:47 -0500

    Modulo de especialidades listo

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import UsuarioRutas from "./src/routes/usuarios.route.js";
 import MedicoRutas from "./src/routes/medicos.route.js";
+import EspecialidadRutas from "./src/routes/especialidades.model.js";
 
 // Instancia global
 const app = express();
@@ -13,5 +14,6 @@ app.use(express.json());
 //Rutas
 app.use("/api", UsuarioRutas);
 app.use("/api", MedicoRutas);
+app.use("/api", EspecialidadRutas);
 
 export default app;
